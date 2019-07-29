78e25cdb835b7579534d458d7c3b96a835728aa0
SimonSisay <Simonsisay9@gmail.com>
2019-07-29 20:17:05 +0300

    added environment variable for redux-logger

--- src/redux/store.js
+++ src/redux/store.js
@@ -3,7 +3,11 @@ import logger from "redux-logger";
 import rootReducer from "./root-reducer";
 import { persistStore } from "redux-persist";
 
-const middlewares = [logger];
+let middlewares;
+
+if (process.env.NODE_ENV === "development") {
+  middlewares.push(logger);
+}
 
 const store = createStore(rootReducer, applyMiddleware(...middlewares));
 
